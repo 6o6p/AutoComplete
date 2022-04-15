86b0fb46d9b4f44bb9dd35e3025170e2b31c106c
Фадихин Дмитрий <fadixin@gmail.com>
2022-04-15 21:50:38 +0400

    After codereview
    комментарии разработчика Контур'а

--- AutoComplete.cs
+++ AutoComplete.cs
@@ -13,8 +13,9 @@ namespace AutoComplete
         {
             var result = new List<string>(3);
 
-            if (!string.IsNullOrEmpty(Surname))
-                result.Add(Surname.Trim());
+            if (!string.IsNullOrEmpty(Surname)) //Кажется, если name состоит из пробелов, то в выводе будет два пробела :) Тут бы подошел string.IsNullOrWhitespace()
+                result.Add(Surname.Trim());     
+
 
             if (!string.IsNullOrEmpty(Name))
                 result.Add(Name.Trim());
@@ -28,7 +29,7 @@ namespace AutoComplete
 
     public class AutoCompleter
     {
-        private readonly List<string> _fullNames = new List<string>();
+        private readonly List<string> _fullNames = new List<string>(); //Можно было бы воспользоваться структурой SortedList, чтобы не сортировать при каждом добавлении
 
         public void AddToSearch(List<FullName> fullNames)
         {
@@ -42,7 +43,7 @@ namespace AutoComplete
 
         public List<string> Search(string prefix)
         {
-            if (prefix.Length > 100 || prefix.Trim() == String.Empty)
+            if (prefix.Length > 100 || prefix.Trim() == String.Empty) //string.IsNullOrWhitespace()
                 throw new ArgumentOutOfRangeException("Запрос не может быть пустым или иметь длину больше 100 символов");
 
             var leftBorder = GetLeftBorder(prefix);
@@ -52,8 +53,8 @@ namespace AutoComplete
         }
 
         private int GetLeftBorder(string prefix) => 
-              ~GetIndexByPredicate(prefix, (itemA, itemB) => 
-                    string.Compare(itemA, itemB, StringComparison.OrdinalIgnoreCase) >= 0
+              ~GetIndexByPredicate(prefix, (itemA, itemB) =>      //Можно было бы реализовать компаратор, было бы эффективнее.
+                    string.Compare(itemA, itemB, StringComparison.OrdinalIgnoreCase) >= 0 
               );
 
         private int GetRightBorder(string prefix) =>
@@ -64,55 +65,5 @@ namespace AutoComplete
 
         private int GetIndexByPredicate(string prefix, Func<string, string, bool> predicate) =>
             _fullNames.BinarySearch(prefix, Comparer<string>.Create((itemA, itemB) => predicate(itemA, itemB) ? 1 : -1));
-
-
-        ///// <summary>
-        ///// Возвращает индекс первого элемента, который содержит <paramref name="prefix"/>. 
-        ///// Если такого элемента нет, возвращает число элементов в коллекции поиска.
-        ///// Регистр игнорируется.
-        ///// Не очень красиво настроен предикат. Работает. Тесты проходит.
-        ///// </summary>
-        ///// <param name="prefix"></param>
-        ///// <returns></returns>
-        //private int GetLeftBorder(string prefix) => GetIndexOfElementToRightOfPredicate(prefix, index =>
-        //    string.Compare(_fullNames[index], prefix, StringComparison.OrdinalIgnoreCase) < 0
-        //);
-
-        ///// <summary>
-        ///// Возвращает индекс первого элемента, который не содержит <paramref name="prefix"/> и лексикографически больше него. 
-        ///// Если такого элемента нет, возвращает ноль.
-        ///// Регистр игнорируется.
-        ///// Не очень красиво настроен предикат. Работает. Тесты проходит.
-        ///// </summary>
-        ///// <param name="prefix"></param>
-        ///// <returns></returns>
-        //private int GetRightBorder(string prefix) => GetIndexOfElementToRightOfPredicate(prefix, index =>
-        //    _fullNames[index].StartsWith(prefix, StringComparison.OrdinalIgnoreCase)
-        // || string.Compare(_fullNames[index], prefix, StringComparison.OrdinalIgnoreCase) < 0
-        //);
-
-        ///// <summary>
-        ///// Производит бинарный поиск <paramref name="prefix"/> по коллекции с условием <paramref name="predicate"/>.
-        ///// </summary>
-        ///// <param name="prefix"></param>
-        ///// <param name="predicate"></param>
-        ///// <returns></returns>
-        //private int GetIndexOfElementToRightOfPredicate(string prefix, Predicate<int> predicate)
-        //{
-        //    var left = -1;
-        //    var right = _fullNames.Count;
-        //    var middle = 0;
-
-        //    while (left + 1 != right)
-        //    {
-        //        middle = left + (right - left) / 2;
-
-        //        if (predicate(middle))
-        //            left = middle;
-        //        else
-        //            right = middle;
-        //    }
-        //    return right;
-        //}
     }
 }
